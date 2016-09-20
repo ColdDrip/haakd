ab40930d46eaa2a456f89ba1561dc04f7bd657e7
alyssadisalvo <alyssadisalvo@d-i89-144-183.student.eduroam.uq.edu.au>
2016-09-20 20:57:26 +1000

    stuff works

--- js/show_orbs.js
+++ js/show_orbs.js
@@ -30,15 +30,15 @@ jQuery(document).ready(function($){
 			 //console.log(shelf_spot);
 			 }
 			 taken_orbs.push(shelf_spot);
-			//document.getElementsByClassName(shelf_spot).attr("id", orbs[i][0]);
+			document.getElementsByClassName(shelf_spot).attr("id", orbs[i][0]);
 
 		}
 
 		console.log("taken orbs", taken_orbs);
 
 			var orbs_to_hide = [];
-			 	for (var j=0; j<121; j++) {
-			 		orbs_to_hide.push((j));
+			 	for (var j=1; j<121; j++) {
+			 		orbs_to_hide.push(j);
 				}
 
 			for (var m in orbs_to_hide) {
@@ -53,16 +53,28 @@ jQuery(document).ready(function($){
 
 			console.log("orbs to hide", orbs_to_hide);
 
-			var orbs_to_hide1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+			// var taken_orbs1 = [2,5,7,8]
+			// var orbs_to_hide1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+			//
+			//
+			//
+			// for (var m1 in orbs_to_hide1) {
+			// 	for (var k1 in taken_orbs1) {
+			// 		//console.log(k);
+			// 		//console.log(m);
+			// 		if ((taken_orbs1[k1]) == (orbs_to_hide1[m1])) {
+			// 		 	delete orbs_to_hide1[m1];
+			// 		}
+			// 	}
+			// }
 
-			for (var y=0; y<6; y++) {
-				delete orbs_to_hide1[y];
-			}
+			console.log(orbs_to_hide);
 
-			for (var p in orbs_to_hide1) {
-			 		$("." + orbs_to_hide1[p]).css();
-					console.log(orbs_to_hide1[p], "was hidden");
-				}  
+			for (var p in orbs_to_hide) {
+			 		$("." + orbs_to_hide[p]).css("opacity", "0");
+					$("." + orbs_to_hide[p]).attr("onclick", "return false");
+					console.log(orbs_to_hide[p], "was hidden");
+				}
 				//$(".2").hide();
 
 
