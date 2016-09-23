0622ed6715041c2198e20f42d9b9880bfa493780
kevin-nkw <kwng.kevin@gmail.com>
2016-09-23 13:09:11 +1000

    testing chute

--- js.js
+++ js.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 
   //initialise draggables
   $(".button").draggable({revert:true});
+  //  $(".button").hover(function(){
+  //    console.log($(this).attr("class").split(' ')[1]);
+  //  });
 
 
   //prevent form submit on adding a dream
