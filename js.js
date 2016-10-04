49683f4510849c0e3059b8c540a8c4ea2db5a0a0
kevin-nkw <kwng.kevin@gmail.com>
2016-10-04 17:07:32 +1000

    testing view dream

--- js.js
+++ js.js
@@ -14,37 +14,33 @@ $(document).ready(function(){
   $(".chute").droppable({drop:function(event,ui){
     $(this).addClass("debug");
     $('#myModal').modal({keyboard: false});
-    //getting the dream data from the php file
-    $.get("../lib/view_dream.php", function(data,status){
-      dream_data = JSON.parse(data, ",");
-      orbID = dream_data[0];
-      Story = dream_data[1];
-      Emotion = dream_data[2];
-      Image_Path_1 = dream_data[3];
-      Image_Path_2 = dream_data[4];
-      consol(orbID, Story, Emotion, Image_Path_1, Image_Path_2);
-      display_dream(orbID, Story, Emotion, Image_Path_1, Image_Path_2);
-    });
 
+    $.get(
+      "../lib/view_dream.php",function(data,status){
+        dream_data = JSON.parse(data, ",");
+        orbID = dream_data[0];
+        Story = dream_data[1];
+        Emotion = dream_data[2];
+        Image_Path_1 = dream_data[3];
+        Image_Path_2 = dream_data[4];
+        display_dream(orbID, Story, Emotion, Image_Path_1, Image_Path_2);
+      });
+      function display_dream(orbID, Story, Emotion, Image_Path_1, Image_Path_2) {
+      	var page_element = "";
+      	page_element += "<div>";
+      	page_element += "<p>" + orbID + "</p>";
+      	page_element += "<p>" + Story + "</p>";
+      	page_element += "<p>" + Emotion + "</p>";
+      	page_element += "<p>" + "<img src=" + Image_Path_1 + ">" + "</p>";
+      	page_element += "<p>" + "<img src=" + Image_Path_2 + ">" + "</p>";
+      	page_element += "</div>";
 
-    //outputting the dream data for the html page
-    //edit here to change the css for view_dream.html
-    function display_dream(orbID, Story, Emotion, Image_Path_1, Image_Path_2) {
-    	var page_element = "";
-    	page_element += "<div>";
-    	page_element += "<p>" + orbID + "</p>";
-    	page_element += "<p>" + Story + "</p>";
-    	page_element += "<p>" + Emotion + "</p>";
-    	page_element += "<p>" + "<img src=" + Image_Path_1 + ">" + "</p>";
-    	page_element += "<p>" + "<img src=" + Image_Path_2 + ">" + "</p>";
-    	page_element += "</div>";
+      	var html = $.parseHTML(page_element);
+      	//console.log(html);
+      	$('#myModal .modal-body').append(html);
 
-    	var html = $.parseHTML(page_element);
-    	//console.log(html);
-    	$('#myModal .modal-body').append(html);
-
-    }
-
+      }
+  }
   });
 
 //revert orb to original position on modal close
