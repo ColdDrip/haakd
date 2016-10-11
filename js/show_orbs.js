c0749400a7ba4b79ba3c0abd53b794516b853a22
kevin-nkw <kwng.kevin@gmail.com>
2016-10-12 00:43:32 +1000

    orb fix

--- js/show_orbs.js
+++ js/show_orbs.js
@@ -136,9 +136,8 @@ function special() {
 						console.log("the array id", taken_orbs[b][3]);
 						console.log("the max", maximum_id);
 						//make the css change you want here
-						$(".button." + taken_orbs[b][0]).animate({"border", "1px solid white"},200,function(){});
-						$(".button." + taken_orbs[b][0]).animate({"content", "new"},200,function(){});
-						$(".button." + taken_orbs[b][0]).css("color":"blue");
+						$(".button." + taken_orbs[b][0]).animate({border: "1px solid white"},200);
+						$(".button." + taken_orbs[b][0]).animate({content: "new"},200);
 
 
 
