c4c4e8ba7eae41c69ee6bc6120cb623c72824f02
alyssadisalvo <alyssadisalvo@d-i89-144-32.student.eduroam.uq.edu.au>
2016-10-15 16:22:24 +1000

    modals working

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-  $('#myModal').modal({keyboard: false});
 
+$('#about1').click(function(){
+  $('#about').modal({keyboard: false});
+});
+
+$('#how_it_works1').click(function(){
+  $('#how_it_works').modal({keyboard: false});
+});
+
+$('#dreamtime1').click(function(){
+  $('#dreamtime').modal({keyboard: false});
+});
 
 });
