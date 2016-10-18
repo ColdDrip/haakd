d894eddd5a532e5dbf3a4343f0a81a5cfb688891
kevin-nkw <kwng.kevin@gmail.com>
2016-10-19 00:09:56 +1000

    show orb color fix

--- js/show_orbs.js
+++ js/show_orbs.js
@@ -130,14 +130,14 @@ special();
 					if (type="normal"){
 					for (h in orb_data) {
 						console.log(orb_data);
-						$(".button." + orb_data[h][0]).css("box-shadow", emo_color[orb_data[h][1]][0]);
-						$(".button." + orb_data[h][0]).css("background",emo_color[orb_data[h][1]][1]);
+						$(".button ." + orb_data[h][0]).css("box-shadow", emo_color[orb_data[h][1]][0]);
+						$(".button ." + orb_data[h][0]).css("background",emo_color[orb_data[h][1]][1]);
 					}
 				} else if(type="myorbs"){
 					for (h in orb_data) {
 						console.log(orb_data);
-						$(".button." + my_shelf_spot[h]).css("box-shadow", emo_color[orb_data[h][1]][0]);
-						$(".button." + my_shelf_spot[h]).css("background",emo_color[orb_data[h][1]][1]);
+						$(".button ." + my_shelf_spot[h]).css("box-shadow", emo_color[orb_data[h][1]][0]);
+						$(".button ." + my_shelf_spot[h]).css("background",emo_color[orb_data[h][1]][1]);
 					}
 				}
 				}
