03245bbf681a03b0835c3bc2ad457c8809a62dcb
kevin-nkw <kwng.kevin@gmail.com>
2016-10-22 19:21:48 +1000

    fix

--- js/add_dream.js
+++ js/add_dream.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
     } else if (stepcount==2){
       stepcount=3;
       $(".guidetext").empty();
-      $(".guidetext").append("Select te pieces of artwork to visually illustrate your dream.");
+      $(".guidetext").append("Select two pieces of artwork to visually illustrate your dream.");
     $("#newDreamSubmit").fadeOut(1000);
     $(".emotion-control").fadeOut(1000,function(){
       $(".searchbox") .fadeIn(1000);
@@ -166,47 +166,53 @@ $('#back').click(function(event){
 
       updateURL(imageNum,imageNum2);
       printImages();
+
+      //when refreshing image1
+      $("a#changeImages1").click(function changeImages(event) {
+        event.preventDefault();
+        $('#output1').empty();
+
+        imageNum = Math.floor((Math.random()*loadedImages.length));
+        if (imageNum == imageNum2){
+          imageNum = imageNum + 1;
+          console.log("change1-image1",imageNum);
+          console.log("change1-image2",imageNum2);
+          printImages();
+        }else{
+          console.log("change1-1-image1",imageNum);
+          console.log("change1-1-image2",imageNum2);
+          console.log(contributor1,issued1,title1);
+          console.log(contributor2,issued2,title2);
+          updateURL(imageNum,imageNum2);
+          printImages();
+
+        }
+      });
+
+
+      //when refreshing image2
+      $("a#changeImages2").click(function changeImages2(event) {
+        event.preventDefault();
+        $('#output2').empty();
+        imageNum2 = Math.floor((Math.random()*loadedImages.length));
+        if (imageNum == imageNum2){
+          imageNum2 = imageNum2+1;
+          console.log("change2-image1",imageNum);
+          console.log("change2-image2",imageNum2);
+          printImages();
+        }else{
+          console.log("change2-1-image1",imageNum);
+          console.log("change2-1-image2",imageNum2);
+          console.log(contributor1,issued1,title1);
+          console.log(contributor2,issued2,title2);
+          updateURL(imageNum,imageNum2);
+          printImages();
+        }
+      });
     });
   });
 
-  //when refreshing image1
-  $("a#changeImages1").click(function changeImages(event) {
-    event.preventDefault();
-    $('#output1').empty();
 
-    imageNum = Math.floor((Math.random()*loadedImages.length));
-    if (imageNum == imageNum2){
-      imageNum = imageNum + 1;
-      console.log("change1-image1",imageNum);
-      console.log("change1-image2",imageNum2);
-      printImages();
-    }else{
-      console.log("change1-1-image1",imageNum);
-      console.log("change1-1-image2",imageNum2);
-      updateURL(imageNum,imageNum2);
-      printImages();
-
-    }
-  });
-
-
-  //when refreshing image2
-  $("a#changeImages2").click(function changeImages2(event) {
-    event.preventDefault();
-    $('#output2').empty();
-    imageNum2 = Math.floor((Math.random()*loadedImages.length));
-    if (imageNum == imageNum2){
-      imageNum2 = imageNum2+1;
-      console.log("change2-image1",imageNum);
-      console.log("change2-image2",imageNum2);
-      printImages();
-    }else{
-      console.log("change2-1-image1",imageNum);
-      console.log("change2-1-image2",imageNum2);
-      updateURL(imageNum,imageNum2);
-      printImages();
-    }
-  });
 
   //add two images URL into array and print them out
   function updateURL(imageNum,imageNum2){
